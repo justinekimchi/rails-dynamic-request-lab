f27c77f277dd9d13512fdae040d18b138d28c7d9
justinekimchi <justine.kim.manaloto@gmail.com>
2020-11-01 21:16:45 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,10 +6,10 @@ class StudentsController < ApplicationController
     def show
         @student = Student.find_by(id: params[:id])
 
-        if @student
-            render :show
-        else
-            redirect_to '/students'
-        end
+        # if @student
+        #     render :show
+        # else
+        #     redirect_to '/students'
+        # end
     end
 end
